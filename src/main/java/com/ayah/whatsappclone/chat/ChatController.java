57abf67ac826c3de5ba3ca0e26e9cb99429e7590
Ayah Alkhdarat <ayah.alkhdarat@digitinary.com>
2026-03-09 17:49:01 +0300

    add swagger

--- src/main/java/com/ayah/whatsappclone/chat/ChatController.java
+++ src/main/java/com/ayah/whatsappclone/chat/ChatController.java
@@ -1,5 +1,6 @@
 package com.ayah.whatsappclone.chat;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/chats")
 @RequiredArgsConstructor
+@Tag(name = "Chat")
 public class ChatController {
 
     private final ChatService chatService;
--- src/main/java/com/ayah/whatsappclone/message/MessageController.java
+++ src/main/java/com/ayah/whatsappclone/message/MessageController.java
@@ -1,5 +1,7 @@
 package com.ayah.whatsappclone.message;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/messages")
 @RequiredArgsConstructor
+@Tag(name = "Message")
 public class MessageController {
 
     private final MessageService messageService;
@@ -32,6 +35,7 @@ public class MessageController {
     @PostMapping(value = "/upload-media", consumes = "multipart/form-data")
     @ResponseStatus(HttpStatus.CREATED)
     public void uploadMedia(@RequestParam("chatId") String chatId,
+                            @Parameter()
                             @RequestParam("file") MultipartFile file,
                             Authentication authentication) {
 
